862f70e836ae6a04c721b6382a4a3652f54f3a0f
Alvar Sokk <alvar195@hotmail.com>
2018-07-09 16:17:53 +0300

    Redux devtools fix

--- frontend/src/store/root.js
+++ frontend/src/store/root.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import io from 'socket.io-client';
 import createSocketIoMiddleware from 'redux-socket.io';
 import { CONN_STATUS } from "../store/actions/actionTypes";
 
 import socketReducer from './reducers/socket';
 import userReducer from './reducers/user';
+import connect from "react-redux/es/connect/connect";
 
 let socket = io(process.env.REACT_APP_SOCKET_ADDR);
 let socketIoMiddleware = createSocketIoMiddleware(socket, "server/");
@@ -14,11 +15,12 @@ const rootReducer = combineReducers({
 	user: userReducer
 });
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const reduxDevTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null;
 
 const store = createStore(
 	rootReducer,
-	composeEnhancers(applyMiddleware(socketIoMiddleware))
+	reduxDevTools,
+	applyMiddleware(socketIoMiddleware)
 );
 
 // listens to stuff sent from server
