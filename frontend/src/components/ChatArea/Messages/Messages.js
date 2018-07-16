ce29d706cf9044cf39ff3d87e837511adfebff30
martimutso <marti.mutso@gmail.com>
2018-07-16 13:19:14 +0300

    Added names to message bubbles

--- frontend/src/components/ChatArea/Messages/Messages.js
+++ frontend/src/components/ChatArea/Messages/Messages.js
@@ -12,18 +12,18 @@ const Messages = (props) => {
 			spacing={16}
 			xs={12}>
 			{/* Hardcoded placeholder */}
-			<Grid item xs={12} ><Message message="Cool stuff 24" justify='flex-end' color='primary' /></Grid>
-			<Grid item xs={12} ><Message message="Cool stuff 23" justify='flex-start' /></Grid>
-			<Grid item xs={12} ><Message message="Cool stuff 22" justify='flex-end' color='primary' /></Grid>
-			<Grid item xs={12} ><Message message="Cool stuff 21" justify='flex-start' /></Grid>
-			<Grid item xs={12} ><Message message="Cool stuff 20" justify='flex-end' color='primary' /></Grid>
-			<Grid item xs={12} ><Message message="Cool stuff 19" justify='flex-start' /></Grid>
-			<Grid item xs={12} ><Message message="Cool stuff 18" justify='flex-end' color='primary' /></Grid>
-			<Grid item xs={12} ><Message message="Cool stuff 17" justify='flex-start' /></Grid>
-			<Grid item xs={12} ><Message message="Cool stuff 16" justify='flex-end' color='primary' /></Grid>
-		
-	
-		
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 24" justify='flex-end' color='primary' /></Grid>
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 23" justify='flex-start' /></Grid>
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 22" justify='flex-end' color='primary' /></Grid>
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 21" justify='flex-start' /></Grid>
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 20" justify='flex-end' color='primary' /></Grid>
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 19" justify='flex-start' /></Grid>
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 18" justify='flex-end' color='primary' /></Grid>
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 17" justify='flex-start' /></Grid>
+			<Grid item xs={12} ><Message owner="Vello" message="Cool stuff 16" justify='flex-end' color='primary' /></Grid>
+
+
+
 		</Grid>
 	);
 };
